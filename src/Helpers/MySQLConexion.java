24ccadc1210d0274dab8494bdad3fc43ce86e6d6
Belen <belenfernandezgayral@gmail.com>
2023-09-02 11:39:26 -0300

    Agregados helpers de conexión y creación de tablas

--- src/Helpers/MySQLConexion.java
+++ src/Helpers/MySQLConexion.java
@@ -1,5 +1,39 @@
 package Helpers;
 
-public class MySQLConexion {
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySQLConexion {
+	private static String driver = "com.mysql.cj.jdbc.Driver";
+	private static String uri = "jdbc:mysql://localhost:3306/tp1?createDatabaseIfNotExist=true";
+	private static String user = "root";
+	private static String password = "admin";
+	
+	private static Connection conexion;
+	
+	public static Connection getConexion() {
+		if (conexion == null) {
+			instanciar();
+		}
+		return conexion;
+	}
+	
+	public static void instanciar() {
+		try {
+			Class.forName(driver).getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		try {
+			Connection conn = DriverManager.getConnection(uri, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 }
--- src/Helpers/MySQLTablas.java
+++ src/Helpers/MySQLTablas.java
@@ -1,5 +1,63 @@
 package Helpers;
 
-public class MySQLTablas {
+import java.sql.SQLException;
 
+public class MySQLTablas {
+	
+	public static void createTablaCliente() {
+		String table = "CREATE TABLE IF NOT EXISTS cliente("
+						+ "idCliente INT,"
+						+ "nombre VARCHAR(500),"
+						+ "email VARCHAR(150),"
+						+ "PRIMARY KEY(idCliente))";
+		
+		try {
+			MySQLConexion.getConexion().prepareStatement(table).execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createTablaFactura() {
+		String table = "CREATE TABLE IF NOT EXISTS factura("
+						+ "idFactura INT,"
+						+ "idCliente INT,"
+						+ "PRIMARY KEY(idFactura),"
+						+ "FOREIGN KEY(idCliente) REFERENCES cliente(idCliente))";
+		
+		try {
+			MySQLConexion.getConexion().prepareStatement(table).execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createTablaFacturaProducto() {
+		String table = "CREATE TABLE IF NOT EXISTS factura_producto("
+						+ "idFactura INT,"
+						+ "idProducto INT,"
+						+ "cantidad INT,"
+						+ "FOREIGN KEY (idFactura) REFERENCES factura(idFactura),"
+						+ "FOREIGN KEY (idProducto) REFERENCES producto(idProducto))";
+		
+		try {
+			MySQLConexion.getConexion().prepareStatement(table).execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createTablaProducto() {
+		String table = "CREATE TABLE IF NOT EXISTS producto("
+						+ "idProducto INT,"
+						+ "nombre VARCHAR(45),"
+						+ "valor FLOAT,"
+						+ "PRIMARY KEY(idProducto))";
+		
+		try {
+			MySQLConexion.getConexion().prepareStatement(table).execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
